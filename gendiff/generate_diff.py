e6f8b55c368a39c336efc5b935a6d89004412d4d
Anatoliy <anatoliy.klimanov@gmail.com>
2024-06-04 11:17:03 +0300

    import os in open file

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,20 +1,11 @@
 from gendiff.choose_formatters.choose_formatt import choose_formatt
-from gendiff.open_file import get_open_file_json, get_open_file_yaml
+from gendiff.open_file import get_file_permission
 from gendiff.compare_trees import compare_trees
 
 
 def generate_diff(file1_path, file2_path, stylish='stylish'):
-    if file1_path[-4:] == 'json' and file2_path[-4:] == 'json':
-        first_file = get_open_file_json(file1_path)
-        second_file = get_open_file_json(file2_path)
-    elif file1_path[-4:] == 'yaml' and file2_path[-4:] == 'yaml':
-        first_file = get_open_file_yaml(file1_path)
-        second_file = get_open_file_yaml(file2_path)
-    elif file1_path[-3:] == 'yml' and file2_path[-3:] == 'yml':
-        first_file = get_open_file_yaml(file1_path)
-        second_file = get_open_file_yaml(file2_path)
-    else:
-        return 'The file is selected incorrectly!'
+    first_file = get_file_permission(file1_path)
+    second_file = get_file_permission(file2_path)
     parsing = compare_trees(first_file, second_file)
     result = choose_formatt(parsing, stylish)
     return result
--- gendiff/open_file.py
+++ gendiff/open_file.py
@@ -1,5 +1,6 @@
 import json
 import yaml
+import os
 
 
 def get_open_file_json(file_path):
@@ -10,3 +11,13 @@ def get_open_file_json(file_path):
 def get_open_file_yaml(file_path):
     file = yaml.safe_load(open(file_path, 'r'))
     return file
+
+
+def get_file_permission(file_path):
+    permission = os.path.splitext(file_path)
+    if '.json' in permission:
+        return get_open_file_json(file_path)
+    elif '.yaml' in permission or '.yml' in permission:
+        return get_open_file_yaml(file_path)
+    else:
+        return 'The file is selected incorrectly!'
